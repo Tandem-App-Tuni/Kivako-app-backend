26d83225d130516db7d19ce891095430399f38ad
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2019-11-02 22:47:38 +0200

    Mocking user login

--- src/auth/auth.js
+++ src/auth/auth.js
@@ -1,5 +1,14 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
 
 
+
+
+//
 const isAuthenticated = async (req, res, next) => {
     let userlog = true;
 
--- src/configs/app.js
+++ src/configs/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+var passport = require('passport');
+var SamlStrategy = require('passport-saml').Strategy;
+var saml = require('passport-saml');
+var fs = require('fs');
 
 module.exports = function () {
     let server = express(),
@@ -23,7 +31,88 @@ module.exports = function () {
         server.use(bodyParser.urlencoded({
             extended: false
         }));
-        server.use(express.urlencoded({extended:false}))
+
+        server.use(express.urlencoded({extended:false}));
+        server.use(cookieParser());
+        server.use(express.static(path.join(__dirname,'public')));
+
+        //server.use(session({secret: 'stuff'})); 
+
+
+        // set up passport
+        // ============================================
+
+        server.use(session({// TODO -> CHANGE SECRET TO SECURE FILE
+            secret: 'stuff',
+            proxy: true,
+            resave: true,
+            saveUninitialized: true
+        }));
+
+        passport.serializeUser(function(user, done) {
+            console.log("serializing user");
+            console.log(user)
+            done(null, user);
+        });
+              
+        passport.deserializeUser(function(user, done) {
+            console.log("Deserializing user");
+            //console.log(user)
+            done(null, user);
+        });
+
+        var samlStrategy = new saml.Strategy({
+            callbackUrl: 'http://localhost/login/callback',
+            entryPoint: 'http://localhost:8080/simplesaml/saml2/idp/SSOService.php',
+            issuer: 'saml-poc',
+            identifierFormat: null,
+            decryptionPvk: fs.readFileSync(__dirname + '/auth/key.pem', 'utf8'),
+            privateCert: fs.readFileSync(__dirname + '/auth/key.pem', 'utf8'),
+            validateInResponseTo: false,
+            disableRequestedAuthnContext: true
+          }, function(profile, done) {
+              return done(null, profile);
+        });
+
+        passport.use('samlStrategy', samlStrategy);
+
+        server.use(passport.initialize()); 
+        server.use(passport.session());
+
+        server.get('/login',
+            function (req, res, next) {
+                console.log('-----------------------------');
+                console.log('/Start login handler');
+                next();
+            },
+            passport.authenticate('samlStrategy'),
+        );
+
+        server.post('/login/callback',
+            function (req, res, next) {
+                console.log('-----------------------------');
+                console.log('/Start login callback ');
+                next();
+            },
+            passport.authenticate('samlStrategy'),
+            function (req, res) {
+                console.log('-----------------------------');
+                console.log('login call back dumps');
+                console.log(req.user);
+                console.log(req.user.email)
+                console.log('-----------------------------');
+                res.send('Log in Callback Success');
+            }
+        );
+
+        server.get('/login/check',
+        function(req,res){
+            console.log("User logged");
+            console.log(req.user.email);
+            res.send(req.isAuthenticated());
+        });
+        // =============================================
+
 
         //connect the database
         mongoose.connect(
@@ -40,7 +129,7 @@ module.exports = function () {
         console.log("MongoDB database connection established successfully");
         })
 
-        
+                
         // Set up routes
         routes.init(server);
     };
