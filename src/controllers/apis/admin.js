f5a49004937a60525d1e8d3c8555a4dd5dbc8aa4
João Paulo Nunes Soares <paulonsoares@yahoo.com.br>
2019-12-09 13:45:36 +0200

    Protecting admin pages routes

--- src/controllers/apis/admin.js
+++ src/controllers/apis/admin.js
@@ -3,7 +3,9 @@ const auth = require('../../auth/auth')
 const adminService = require('../../services/admin');
 let router = express.Router();
 
+
+//Get a list with statitics related to the languages
 //http://localhost:3000/api/v1/admin/statitics // GET REQUEST
-router.get('/statitics', adminService.getLanguageStatitics);
+router.get('/statitics', auth.checkIfUserIsAuthenticatedAndAdmin, adminService.getLanguageStatitics);
 
 module.exports = router;
