ab892e0958ce91326527e313fd9de9d3bb6b16e9
Duc Nguyen <duc.nguyen@nordcloud.com>
2020-03-26 21:37:01 +0200

    update image upload function

--- src/services/imageUpload.js
+++ src/services/imageUpload.js
@@ -9,7 +9,7 @@ const storageFolder = constants.uploadsFolder;
 /**
  * The functions here take care of the
  * uploading, storing and serving of user avatars.
- * Avatars are resized uppon storing to a default size of 100 x 100 pixels, retaining aspect ratio.
+ * Avatars are resized uppon storing to a default size of 300 x 300 pixels, retaining aspect ratio.
  * The images are accessible via a get request to the server with a postfixed name of 
  * the user whos avatar is requested.
  */
@@ -18,7 +18,7 @@ const uploadAvatar = async function(req, res, next)
 {
     try
     {
-        sharp(req.file.path).resize(100).toBuffer().then(buf => 
+        sharp(req.file.path).resize(300).toBuffer().then(buf => 
         {
             fs.writeFile(req.file.path, buf, function(error) 
             {
@@ -50,7 +50,7 @@ const getAvatar = async function(req, res, next)
         else imageFile = path.join(storageFolder ,req.user.email);
 
         if (fs.existsSync(imageFile)) res.sendFile(imageFile);
-        else res.status(200).json({message:'No avatar found!'});
+        else res.status(400).json({message:'No avatar found!'});
     }
     catch (error)
     {
