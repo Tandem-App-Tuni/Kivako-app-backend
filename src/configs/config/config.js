8372ba616c7ebac5ee707473efaf19da2f88fa7b
Janaka Nawagamuwa <janaka.nawagamuwa@tuni.fi>
2021-03-20 19:08:24 +0530

    diagnosing change

--- src/configs/config/config.js
+++ src/configs/config/config.js
@@ -1,8 +1,8 @@
 const _ = require('lodash');
-const env = process.env.NODE_ENV || 'local';
-const envConfig = require('./' + env);
+// const env = process.env.NODE_ENV || 'local';
+// const envConfig = require('./' + env);
 const change = 'fake';
-let defaultConfig = {
-  env: env
-};
-module.exports = _.merge(defaultConfig, envConfig);
+// let defaultConfig = {
+//   env: env
+// };
+module.exports = _.merge(change, change);
