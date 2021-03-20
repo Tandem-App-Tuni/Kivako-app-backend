ebb67df3739552cea20affaaea4d78625e531366
Janaka Nawagamuwa <janaka.nawagamuwa@tuni.fi>
2021-03-20 19:01:05 +0530

    test jaming up

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
-const server = require('./configs/app')();
-const config = require('./configs/config/config');
-const db = require('./configs/db');
-const auth = require('./configs/auth/auth')
+// const server = require('./configs/app')();
+// const config = require('./configs/config/config');
+// const db = require('./configs/db');
+// const auth = require('./configs/auth/auth')
 
-//create the basic server setup 
-server.create(config, db);
+// //create the basic server setup 
+// server.create(config, db);
 
-//start the server
-server.start();
+// //start the server
+// server.start();
+
+console.log('here I start server..!!!')
